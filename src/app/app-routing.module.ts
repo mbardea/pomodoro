965e6a0224b95d708e8c2577e461b1dc6496b380
Manuel Bardea <mbardea@gmail.com>
2019-05-31 08:15:04 -0400

    Exploring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TomatoCaseComponent } from './tomato-case/tomato-case.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'timer', component: TomatoCaseComponent },
+    { path: 'settings', component: SettingsComponent },
+    { path: '**', component: TomatoCaseComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridExampleComponent } from './grid-example/grid-example.component';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonBarComponent } from './button-bar/button-bar.component';
+import { TomatoCaseComponent } from './tomato-case/tomato-case.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        GridExampleComponent,
+        HeaderComponent,
+        ButtonBarComponent,
+        TomatoCaseComponent,
+        SettingsComponent,
+        TimerComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatGridListModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
